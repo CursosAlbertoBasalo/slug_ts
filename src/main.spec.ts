9487af0efce228469ce7fe197072cf7e9b682288
Alberto Basalo <albertobasalo@hotmail.com>
2021-06-21 17:30:59 +0200

    test: main test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,12 +1,13 @@
-import { sayHello, sum } from './main';
-
-test('sayHello function exists', () => {
-  expect(sayHello).toBeDefined();
+import { sayHello, sum } from "./main";
+describe("the main program", () => {
+  it("sayHello function exists", () => {
+    expect(sayHello).toBeDefined();
+  });
 });
 
-describe('The sum function', () => {
+describe("the sum function", () => {
   const sut = sum;
-  it('should sum numbers', () => {
+  it("should sum numbers", () => {
     const actual = sut(1, 2);
     const expected = 3;
     expect(actual).toBe(expected);
